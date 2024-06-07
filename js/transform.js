7ec93cd4dc6292ee6d5b5042ad2214e479386e58
Zhenye <2998568539@qq.com>
2024-06-07 14:05:34 +0800

    1

---
+++ js/transform.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+
+  function updateSlide() {
+    slides.forEach((slide, index) => {
+      slide.classList.toggle("active", index === currentSlide);
+    });
+    dots.forEach((dot, index) => {
+      dot.classList.toggle("active", index === currentSlide);
+    });
+  }
+
+  setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateSlide();
+  }, 4000);
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      currentSlide = index;
+      updateSlide();
+    });
+  });
